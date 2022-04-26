529d324d0b6e0a631997d3e4fa173077ed45bdc3
Asim-Coding <muhammadasim19955@gmail.com>
2022-04-26 09:23:52 +0500

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component, VERSION } from '@angular/core';
-
+import { MessageService } from './message.service';
+import { OthermessageService } from './othermessage.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  providers:[MessageService,OthermessageService]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  msg:string;
+  othermsg:string="";
+  //we can inject service in constructor
+  constructor(private _messageservice:MessageService){
+
+  }
+  ngOnInit(){
+  // this.msg=this._messageservice.getmessage();
+  }
+  getmessage(){
+    this.msg=this._messageservice.getmessage();
+    this.othermsg=this._messageservice.getmessagefeomotherservice();
+  }
 }
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { OthermessageService } from './othermessage.service';
 
 @Injectable()
 export class MessageService {
+othermsg:string="";
+  constructor(private _othermessageService:OthermessageService) { }
 
-  constructor() { }
-
+  getmessage(){
+    return 'sahosofttech sol'
+  }
+  getmessagefeomotherservice(){
+    this.othermsg=this._othermessageService.getothermessage();
+    return this.othermsg;
+  }
 }
